0eba0ce82a89e339f4f9991b64dc8c7352b49aba
Destiny911gtr <dhanush911gtr@gmail.com>
2021-11-21 11:56:53 +0530

    Add a page to show contacts

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-
 import LoginPage from './views/LoginPage';
 import Profile from './views/Profile';
 import CameraComponent from './views/CameraView';
--- views/ContactsView.js
+++ views/ContactsView.js
@@ -1,111 +1,86 @@
-import React, {useState} from 'react';
-import {
-  View,
-  Component,
-  Avatar,
-  ListItem,
-  ScrollView,
-  ActivityIndicator,
-  PermissionsAndroid,
-  StyleSheet,
-} from 'react-native';
-import Contacts from 'react-native-contacts';
+// Access Device’s Contact List in React Native App
+// https://aboutreact.com/access-contact-list-react-native/
 
+// Import React
+import React, {useState, useEffect} from 'react';
+
+// Import all required component
+import {
+  PermissionsAndroid,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TextInput,
+} from 'react-native';
+
+import Contacts from 'react-native-contacts';
+import ListView from '../components/ListView';
 import * as colors from '../components/Colors';
 
-export default class ContactsView extends Component {
-  constructor() {
-    this.state = {
-      contacts: [],
-      loading: true
-    };
-  }
+const ContactsView = () => {
+  let [contacts, setContacts] = useState([]);
 
-  async componentDidMount() {
-    if (Platform.OS === "android") {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
       PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS, {
-        title: "Contacts",
-        message: "This app would like to view your contacts."
+        title: 'Contacts',
+        message: 'This app would like to view your contacts.',
       }).then(() => {
-        this.loadContacts();
+        loadContacts();
       });
     } else {
-      this.loadContacts();
+      loadContacts();
     }
-  }
+  }, []);
 
-  loadContacts() {
+  const loadContacts = () => {
     Contacts.getAll()
       .then(contacts => {
-        this.setState({ contacts, loading: false });
+        contacts.sort(
+          (a, b) => a.givenName.toLowerCase() > b.givenName.toLowerCase(),
+        );
+        console.log('contacts -> ', contacts);
+        setContacts(contacts);
       })
-      .catch(e => {
-        this.setState({ loading: false });
-      });
-
-    Contacts.getCount().then(count => {
-      this.setState({ searchPlaceholder: `Search ${count} contacts` });
-    });
-
-    Contacts.checkPermission();
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-
-        {
-          this.state.loading === true ?
-            (
-              <View style={styles.spinner}>
-                <ActivityIndicator size="large" color="#0000ff" />
-              </View>
-            ) : (
-              <ScrollView style={{ flex: 1 }}>
-                {this.state.contacts.map(contact => {
-                  return (
-                    <ListItem
-                      leftElement={
-                        <Avatar
-                          img={
-                            contact.hasThumbnail
-                              ? { uri: contact.thumbnailPath }
-                              : undefined
-                          }
-                          placeholder={getAvatarInitials(
-                            `${contact.givenName} ${contact.familyName}`
-                          )}
-                          width={40}
-                          height={40}
-                        />
-                      }
-                      key={contact.recordID}
-                      title={`${contact.givenName} ${contact.familyName}`}
-                      description={`${contact.company}`}
-                      onPress={() => this.onPressContact(contact)}
-                      onLongPress={() => Contacts.viewExistingContact(contact)}
-                      onDelete={() =>
-                        Contacts.deleteContact(contact).then(() => {
-                          this.loadContacts();
-                        })
-                      }
-                    />
-                  );
-                })}
-              </ScrollView>
-            )
+      .catch(err => {
+        if (err === 'denied') {
+          alert('Permission to access contacts was denied');
+          console.warn('Permission to access contacts was denied');
         }
+      });
+  };
 
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <FlatList
+          data={contacts}
+          renderItem={contact => {
+            {
+              console.log('contact -> ' + JSON.stringify(contact));
+            }
+            return (
+              <ListView
+                key={contact.item.recordID}
+                item={contact.item}
+              />
+            );
+          }}
+          keyExtractor={item => item.recordID}
+        />
       </View>
-    );
-  }
-}
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.backgroundCol,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default ContactsView;
